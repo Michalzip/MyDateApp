6e5be1d82faed81fb239dacbc456153fc28079c2
Michał Potoczny <michal@MacBook-Air-Micha.local>
2023-02-21 16:23:54 +0100

    implement-CQRS

---
+++ DateApp/Controllers/TransactionController.cs
@@ -0,0 +1,51 @@
+﻿
+using Microsoft.AspNetCore.Mvc;
+using DateApp.Repositories.Interfaces;
+using DateApp.Extensions;
+using DateApp.Entities;
+
+namespace App.Controllers
+{
+    [Authorize(Policy = "UserProfile")]
+    public class TransactionController : Controller
+    {
+
+        private readonly ITransactionService _transactionService;
+
+        public TransactionController(ITransactionService transactionService)
+        {
+
+            _transactionService = transactionService;
+
+        }
+
+        [HttpGet("create-transaction")]
+        public async Task<ActionResult> CreateTransaction()
+        {
+
+            await _transactionService.UpdatePreviousTransaction();
+
+            string currentUser = User.GetUsername();
+
+            var rezult = await _transactionService.CreateTransaction(currentUser);
+
+            return Ok(rezult);
+
+        }
+
+
+        [HttpGet("confirm-transaction")]
+        public async Task<ActionResult> ConfirmTransaction([FromQuery] string? payerID, [FromQuery] string? guid)
+        {
+
+
+            string currentUser = User.GetUsername();
+
+            var rezult = await _transactionService.ConfirmTransaction(payerID, guid, currentUser);
+
+            return Ok(rezult);
+
+        }
+
+    }
+}
